aa650f2e402d0a78116ff80285b658ab263e1c29
unknown <habibmisi3@gmail.com>
2022-04-05 16:38:06 +0200

    mysql-connection

--- connect.php
+++ connect.php
@@ -5,12 +5,12 @@ $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
 $cleardb_password = $cleardb_url["pass"];
 $cleardb_db = substr($cleardb_url["path"], 1);
-$dsn = 'mysql:host='+$cleardb_url["host"]+';dbname='+$cleardb_db;
+$dsn = 'mysql:host=' . $cleardb_url["host"] . ';dbname=' . $cleardb_db;
 $username = $cleardb_url["user"];
 $password = $cleardb_url["pass"];
 
 try {
-    $connect = new PDO($dsn , $username ,$password);
+    $connect = new PDO($dsn, $username, $password);
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOExeception $e) {
     echo 'Connection Error here' . $e->getMessage();
